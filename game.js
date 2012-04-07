850266b8cf3680bf5e98e4c3ecca53d430dce8ac
johnblanco <j.pabloblanco@gmail.com>
2012-04-07 19:28:16 -0300

    added stuff

--- game.js
+++ game.js
@@ -1,6 +1,12 @@
 var ctx;
-var x = 100;
-var y = 100;
+var canvas_height;
+var canvas_width;
+var snake = {
+  width: 5,
+  pos: {x: 50, y:50},
+  pieces: [{x:49, y:50},{x:48, y:50},{x:47, y:50},{x:46, y:50},{x:45, y:50}],
+  speed: {x:1, y:0}
+};
 var rightDown = false;
 var leftDown = false;
 var upDown = false;
@@ -8,10 +14,10 @@ var downDown = false;
 
 function drawMap() {
   ctx.fillStyle = "rgb(200,0,0)";
-  ctx.fillRect(0, 0, 800, 2);
-  ctx.fillRect(0, 0, 2, 600);
-  ctx.fillRect(0, 598, 800, 2);
-  ctx.fillRect(798, 0, 2, 600);
+  ctx.fillRect(0, 0, canvas_width, 2);
+  ctx.fillRect(0, 0, 2, canvas_height);
+  ctx.fillRect(0, canvas_height-2, canvas_width, 2);
+  ctx.fillRect(canvas_width-2, 0, 2, canvas_height);
 }
 
 function clear() {
@@ -21,19 +27,47 @@ function clear() {
 function draw(){
   clear();
   drawMap();
+  drawSnake();
 
-  ctx.fillRect(x,y,5,5);
 }
 
+function drawSnake(){
+  //draw snake
+  ctx.fillRect(snake.pos.x, snake.pos.y, snake.width, snake.width);
+  $.each(snake.pieces,function(index, value){
+    ctx.fillRect(value.x, value.y, snake.width, snake.width);
+  });
+}
+
+function checkCollisions() {
+  if (upDown && snake.speed.y != 1)
+    snake.speed = {x: 0, y:-1 };
+  if (downDown && snake.speed.y != -1)
+    snake.speed = {x: 0, y:1 };
+  if (leftDown && snake.speed.x != 1)
+    snake.speed = {x: -1, y:0 };
+  if (rightDown && snake.speed.x != -1)
+    snake.speed = {x: 1, y:0 };
+}
+function updateSnake() {
+  snake.pos.x += snake.speed.x;
+  snake.pos.y += snake.speed.y;
+
+  $.each(snake.pieces, function(index,value){
+    value.x += snake.speed.x;
+    value.y += snake.speed.y;
+  });
+
+  checkCollisions();
+}
 function update(){
-  if(leftDown)
-    x -= 1;
-  if(rightDown)
-    x+=1;
-  if(upDown)
-    y-=1;
-  if(downDown)
-    y+=1;
+
+  updateSnake();
+  if(snake.pos.x >= canvas_width - snake.width || snake.pos.x <= 0)
+    snake.speed.x = 0;
+  if(snake.pos.y <= 0 || snake.pos.y >= canvas_height - snake.width)
+    snake.speed.y = 0;
+  
   draw();
 }
 
@@ -65,7 +99,9 @@ $(document).ready(function(){
 
 
   ctx = $('#canvas')[0].getContext("2d");
-  setInterval(update,10)
+  canvas_width = $("#canvas").attr("width");
+  canvas_height = $("#canvas").attr("height");
+  setInterval(update,25)
   
   
 });
