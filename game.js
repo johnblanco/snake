a52e2f3b205de8af697662e6d2e2e748cfe8b107
Gonzalo Arreche <gonzaloarreche@gmail.com>
2012-04-17 00:43:48 -0300

    Moving Map to another file.

--- game.js
+++ game.js
@@ -1,28 +1,6 @@
 //jquery cdn: http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js
 //jquery local: jquery-1.7.2.js
 
-var Map = function($obj, width, height) {
-  this.width = $obj.attr("width");
-  this.height = $obj.attr("height");
-  this.ctx = $('#canvas')[0].getContext("2d");
-};
-
-Map.prototype.clear = function() {
-  this.ctx.clearRect(0, 0, this.width, this.height);
-};
-
-Map.prototype.draw = function() {
-  this.clear();
-
-  this.ctx.fillStyle = "rgb(200,0,0)";
-  this.ctx.fillRect(0, 0, this.width, 2);
-  this.ctx.fillRect(0, 0, 2, this.height);
-  this.ctx.fillRect(0, this.height - 2, this.width, 2);
-  this.ctx.fillRect(this.width - 2, 0, 2, this.height);
-};
-
-
-
 function rgbToHex(r, g, b) {
   if (r > 255 || g > 255 || b > 255)
     throw "Invalid color component";
